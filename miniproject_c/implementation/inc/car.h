323e056b8d22c7ad4c2e4e7b4bd330cc5f3125a9
99002498 <71481395+99002498@users.noreply.github.com>
2020-09-19 09:50:33 +0530

    Create car.h

---
+++ miniproject_c/implementation/inc/car.h
@@ -0,0 +1,16 @@
+#ifndef __CAR_H__
+#define __CAR_H__
+
+
+#include <stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<conio.h>
+
+
+void registration();
+void login();
+void cardetails();
+
+
+#endif
